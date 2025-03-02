8fd248848afd755d704abcf2b279570aad6ad980
igris-kali <igris-kali@igris.kali>
2025-03-02 16:21:21 +0530

    first commit

---
+++ website/index.js
@@ -0,0 +1,12 @@
+console.log(`hello`);
+console.log(`i like long hair`);
+
+window.alert(`hello world`);
+
+//this is a comment
+
+/* 
+this is a comment */
+
+
+document.getElementById("hh").textContent = `Hello`;
